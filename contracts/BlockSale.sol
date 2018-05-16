89c600d49c6da54392a6638723a5129c28412aae
deltonmyalil <deltonguns@gmail.com>
2018-05-16 20:51:48 +0530

    sellArticle() functionality added. One at a time.

--- contracts/BlockSale.sol
+++ contracts/BlockSale.sol
@@ -1,17 +1,27 @@
 pragma solidity ^0.4.18;
 
 contract BlockSale {
-    //state vars
-    address seller;
-    string name;
-    string description;
-    uint256 price;
+	//state vars
+	address seller;
+	string name;
+	string description;
+	uint256 price; //price is in wei
 
-    //sell an article
-    function sellArticle(string _name, string _description, uint256 _price) public {
-        seller = msg.sender; //msg.sender will give the address of the transaction calling seller
-        name = _name;
-        description = _description;
-        price = _price;
-    }
+	//sell an article
+	function sellArticle(string _name, string _description, uint256 _price) public {
+		seller = msg.sender; //msg.sender will give the address of the transaction calling seller
+		name = _name;
+		description = _description;
+		price = _price;
+	}
+
+	//get article
+	function getArticle() public view returns ( //view means it reads state vars but not write/compute
+		address _seller,
+		string _name,
+		string _description,
+		uint256 _price
+	) {
+		return (seller, name, description, price);
+	}
 }
